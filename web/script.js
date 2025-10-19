f88689d4ef113d7e34acffdd2f491ab12a7cd42c
DulshanSiriwardhana <dulshansiriwardhana70@gmail.com>
2025-10-19 07:48:17 +0530

    web view updated

--- web/script.js
+++ web/script.js
@@ -1,25 +1,31 @@
 async function loadBlockchain() {
-    try {
-        const res = await fetch('../blockchain.json');
-        const chain = await res.json();
+    const res = await fetch('blockchain.json');
+    const blockchain = await res.json();
 
-        const container = document.getElementById('blocks');
-        container.innerHTML = '';
+    const container = document.getElementById('chain');
+    container.innerHTML = '';
 
-        chain.forEach(block => {
-            const div = document.createElement('div');
-            div.className = 'block';
-            div.innerHTML = `
-                <b>Index:</b> ${block.index}<br>
-                <b>Data:</b> ${block.data}<br>
-                <b>Hash:</b> <code>${block.hash}</code><br>
-                <b>Previous Hash:</b> <code>${block.previousHash}</code>
-            `;
-            container.appendChild(div);
-        });
-    } catch (err) {
-        console.error("Failed to load blockchain.json", err);
-    }
+    const keys = Object.keys(blockchain);
+    keys.forEach((key, idx) => {
+        const block = blockchain[key];
+
+        const blockDiv = document.createElement('div');
+        blockDiv.className = 'block';
+        blockDiv.innerHTML = `
+            <b>Index:</b> ${block.index}<br>
+            <b>Data:</b> ${block.data}<br>
+            <b>Hash:</b> <code>${block.hash}</code><br>
+            <b>PrevHash:</b> <code>${block.previousHash}</code>
+        `;
+        container.appendChild(blockDiv);
+
+        // Only add arrow if next block exists on the same row
+        if (idx < keys.length - 1) {
+            const arrowDiv = document.createElement('div');
+            arrowDiv.className = 'arrow';
+            container.appendChild(arrowDiv);
+        }
+    });
 }
 
 loadBlockchain();
